f07fb1c5cb469e5965b410f82d4a9a4f0231b963
d6som <ektha9127@naver.com>
2023-08-03 11:03:13 +0900

    fix...ing

---
+++ pjec15/src/rtl/RCA_8bit.v
@@ -0,0 +1,15 @@
+module RCA_8bit(
+    input [7:0] a, b;
+    input cin,
+    output [7:0] sum,
+    output cout,
+);
+
+wire carry;
+
+RCA_4bit RCA_4bit_1( .a(a[3:0]), .b(b[3:0]), .cin(cin), .sum(sum[3:0]), .cout(carry));
+RCA_4bit RCA_4bit_2( .a(a[7:4]), .b(b[7:4]), .cin(carry), .sum(sum[7:4]), .cout(cout));
+
+endmodule 
+
+
