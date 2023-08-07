509dc1010c325877fb9a32d087e71f36364d0646
d6som <ektha9127@naver.com>
2023-08-07 13:23:14 +0900

    CLA

--- pjec16/src/rtl/piece_CLA.v
+++ pjec16/src/rtl/piece_CLA.v
@@ -6,7 +6,7 @@ module cla_piece(
     output [7:0] carry 
 );
 
-assign c[0] = g[0] | (p[0]&cin);
+assign c[0] = g[0] | (p[0] & cin);
 assign c[1] = g[1] | (p[1] & carry[0]);
 assign c[2] = g[2] | (p[2] & carry [1]);
 assign c[3] = g[3] | (p[3] & carry [2]);
